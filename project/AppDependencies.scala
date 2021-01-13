9df7084158ab8594ce2e5efee4356e30166651a4
mikail-khan <mikail.khan@digital.hmrc.gov.uk>
2021-01-13 16:51:28 +0000

    add required test deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,10 +11,19 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "3.3.0" % Test,
-    "org.scalatest"           %% "scalatest"                % "3.2.3"  % Test,
-    "com.typesafe.play"       %% "play-test"                % current  % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8" % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"  % "test, it"
+    //original
+//    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "3.3.0" % Test,
+//    "org.scalatest"           %% "scalatest"                % "3.2.3"  % Test,
+//    "com.typesafe.play"       %% "play-test"                % current  % Test,
+//    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8" % "test, it",
+//    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"  % "test, it"
+  "uk.gov.hmrc"                %% "bootstrap-test-play-27"     % "3.0.0"          % Test,
+  "org.scalatest"              %% "scalatest"                  % "3.2.3"          % Test,
+  "com.typesafe.play"          %% "play-test"                  % current          % Test,
+  "com.vladsch.flexmark"        % "flexmark-all"               % "0.36.8"         % "test, it",
+  "org.scalatestplus.play"     %% "scalatestplus-play"         % "4.0.3"          % "test, it",
+  "org.scalamock"              %% "scalamock"                  % "4.4.0"          % "test",
+  "com.github.alexarchambault" %% "scalacheck-shapeless_1.14"  % "1.2.3"          % "test",
+  "uk.gov.hmrc"                %% "reactivemongo-test"         % "4.22.0-play-27" % "test"
   )
 }
