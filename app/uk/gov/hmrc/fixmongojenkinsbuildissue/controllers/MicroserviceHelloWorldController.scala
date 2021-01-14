522f8fc81fe8afab9f4a70a3301d2178627630c2
mikail-khan <mikail.khan@digital.hmrc.gov.uk>
2021-01-14 10:28:35 +0000

    make warning disappear

--- app/uk/gov/hmrc/fixmongojenkinsbuildissue/controllers/MicroserviceHelloWorldController.scala
+++ app/uk/gov/hmrc/fixmongojenkinsbuildissue/controllers/MicroserviceHelloWorldController.scala
@@ -16,16 +16,15 @@
 
 package uk.gov.hmrc.fixmongojenkinsbuildissue.controllers
 
-import javax.inject.{Inject, Singleton}
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
-import uk.gov.hmrc.fixmongojenkinsbuildissue.config.AppConfig
 
+import javax.inject.{Inject, Singleton}
 import scala.concurrent.Future
 
 @Singleton()
-class MicroserviceHelloWorldController @Inject()(appConfig: AppConfig, cc: ControllerComponents)
-    extends BackendController(cc) {
+class MicroserviceHelloWorldController @Inject()(cc: ControllerComponents)
+  extends BackendController(cc) {
 
   def hello(): Action[AnyContent] = Action.async {
     Future.successful(Ok("Hello world"))
