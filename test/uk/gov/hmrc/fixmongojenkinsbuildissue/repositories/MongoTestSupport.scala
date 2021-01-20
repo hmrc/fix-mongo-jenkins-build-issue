d5c17a7a6946fa22f2e1b0d9588194d052ee36df
Mikail Khan <mikail.khan@digital.hmrc.gov.uk>
2021-01-20 00:20:41 +0000

    remove close mongo connection in afterall

--- test/uk/gov/hmrc/fixmongojenkinsbuildissue/repositories/MongoTestSupport.scala
+++ test/uk/gov/hmrc/fixmongojenkinsbuildissue/repositories/MongoTestSupport.scala
@@ -21,7 +21,6 @@ import play.modules.reactivemongo.ReactiveMongoComponent
 import uk.gov.hmrc.mongo.{MongoConnector, MongoSpecSupport}
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.{FiniteDuration, SECONDS}
 
 trait MongoTestSupport extends MongoSpecSupport with BeforeAndAfterEach with BeforeAndAfterAll { this: Suite ⇒
   val reactiveMongoComponent: ReactiveMongoComponent = new ReactiveMongoComponent {
@@ -33,8 +32,8 @@ trait MongoTestSupport extends MongoSpecSupport with BeforeAndAfterEach with Bef
     mongo().drop()
   }
 
-  abstract override def afterAll(): Unit = {
-    super.afterAll()
-    reactiveMongoComponent.mongoConnector.helper.driver.close(FiniteDuration(10, SECONDS))
-  }
+//  abstract override def afterAll(): Unit = {
+//    super.afterAll()
+//    reactiveMongoComponent.mongoConnector.helper.driver.close(FiniteDuration(10, SECONDS))
+//  }
 }
